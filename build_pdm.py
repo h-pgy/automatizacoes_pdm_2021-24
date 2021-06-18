591d9b9c54c1c73f81155c7cd0a297edb8b0c7d6
h-pgy <hpougy@gmail.com>
2021-06-18 20:27:59 -0300

    salvando mapas no word

--- build_pdm.py
+++ build_pdm.py
@@ -1,7 +1,8 @@
 from docx import Document
 from pdm_builder.ficha_parsing import ParserFichas
 from pdm_builder.doc_builder import TableBuilder
-from pdm_builder.tools import pegar_filtro_alteracoes, sort_fichas, checar_metas_presentes
+from pdm_builder.map_builder import MapBuilder
+from pdm_builder.tools import pegar_filtro_alteracoes, sort_fichas, checar_metas_presentes, get_map_files
 
 
 def build_docx(filtro = None, sort_func = None):
@@ -15,9 +16,20 @@ def build_docx(filtro = None, sort_func = None):
     if sort_func:
         fichas = sort_func(fichas)
     docx = Document()
+    mapbuilder = MapBuilder()
     for ficha in fichas:
-        builder =  TableBuilder(ficha, docx)
-        builder()
+        try:
+            mapbuilder.create_maps(ficha)
+        except Exception as e:
+            print(f"Erro ao gerar os mapas na ficha {ficha['file_original']}")
+            print(e)
+
+        builder = TableBuilder(ficha, docx)
+        docx = builder()
+
+        mapas = get_map_files(ficha['ficha_tecnica']['numero_meta'])
+        for mapa in mapas:
+            docx.add_picture(mapa)
 
     docx.save('teste_6.docx')
 
