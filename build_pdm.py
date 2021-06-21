1db8748535c8527437ec6b94fe3f0e60702dd869
h-pgy <hpougy@gmail.com>
2021-06-21 19:06:52 -0300

    prints para ajudar a debugar e melhorias estilo doc

--- build_pdm.py
+++ build_pdm.py
@@ -1,11 +1,13 @@
 from docx import Document
+from docx.shared import Cm
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 from pdm_builder.ficha_parsing import ParserFichas
 from pdm_builder.doc_builder import TableBuilder
 from pdm_builder.map_builder import MapBuilder
 from pdm_builder.tools import pegar_filtro_alteracoes, sort_fichas, checar_metas_presentes, get_map_files
 
 
-def build_docx(filtro = None, sort_func = None):
+def build_docx(filtro=None, sort_func=None, verbose=True):
 
     parser = ParserFichas('original_data/Devolutiva 11-jun', 'rodada_3')
     fichas = parser()
@@ -17,9 +19,12 @@ def build_docx(filtro = None, sort_func = None):
         fichas = sort_func(fichas)
     docx = Document()
     mapbuilder = MapBuilder()
-    for ficha in fichas:
+    for i, ficha in enumerate(fichas):
+        if verbose:
+            print(i)
         try:
             mapbuilder.create_maps(ficha)
+            print(f"Ficha {ficha['file_original']} parseada com sucesso")
         except Exception as e:
             print(f"Erro ao gerar os mapas na ficha {ficha['file_original']}")
             print(e)
@@ -29,14 +34,15 @@ def build_docx(filtro = None, sort_func = None):
 
         mapas = get_map_files(ficha['ficha_tecnica']['numero_meta'])
         for mapa in mapas:
-            docx.add_picture(mapa)
+            docx.add_picture(mapa, width = Cm(10))
+            last_paragraph = docx.paragraphs[-1]
+            last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-    docx.save('teste_6.docx')
+        docx.add_page_break()
+
+    docx.save('teste_9.docx')
 
 
 if __name__ == '__main__':
-    try:
-        filtro = pegar_filtro_alteracoes()
-        build_docx(filtro, sort_fichas)
-    except Exception as e:
-        print(e)
+
+    build_docx(filtro= None, sort_func = sort_fichas)
